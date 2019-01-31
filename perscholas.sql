057b4d0239ddb3c12b46ea0d9752e812a6af88f3
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2019-01-31 11:41:55 -0500

    Update perscholas.sql

--- perscholas.sql
+++ perscholas.sql
@@ -8,3 +8,10 @@ group by c.customerNumber;
 
 #the relatioship between the customers and orders table is customerNumber
 #we use the count function
+
+select c.contactLastName, c.contactFirstName, count(o.orderNumber) 
+from customers c left join orders o
+on c.customerNumber=o.customerNumber
+group by c.customerNumber
+having count(o.orderNumber) between 5 and 10;
+#having count(o.orderNumber)>=5 and count(o.orderNumber)<=10;
