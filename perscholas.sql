1d1eaddff377636d93a12903578b7057aeceabd2
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2019-01-31 12:00:30 -0500

    Update perscholas.sql

--- perscholas.sql
+++ perscholas.sql
@@ -15,3 +15,16 @@ on c.customerNumber=o.customerNumber
 group by c.customerNumber
 having count(o.orderNumber) between 5 and 10;
 #having count(o.orderNumber)>=5 and count(o.orderNumber)<=10;
+
+select c.customerName, sum(p.amount)
+from customers c join payments p
+on c.customerNumber=p.customerNumber
+group by c.customerNumber;
+
+select c.customerName, sum(p.amount)
+from customers c join payments p
+on c.customerNumber=p.customerNumber
+where p.paymentDate>='2004-01-01'
+group by c.customerNumber;
+#YYYY-MM-DD
+ 
